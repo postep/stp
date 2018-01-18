274f2811cdbfa371f743a71e5a29203472ccdcb6
postep <jakub.postepski@gmail.com>
2018-01-18 15:38:54 +0100

    pid

--- projekt2/matlab/z3.m
+++ projekt2/matlab/z3.m
@@ -1,26 +1,42 @@
 %%regulator pid
-Kk = -0.985;
-Tk = 23;
-Kr = 0.6*Kk; Ti = 0.5*Tk; Td = 0.12*Tk;
-Kr = -0.99; Ti = 0.5*Tk; Td = 0.12*Tk;
-disp(Kr);
-disp(Ti);
-disp(Td);
-T = 0.01;
+Kr = -0.1; Ti = -0.4; Td = -0.05;
+
+T = -0.1;
+y_zad = 1;
+T_sim = 80;
 
 r2 = Kr*(Td/T);
 r1 = Kr*(T/(2*Ti)-2*Td/T-1);
 r0 = Kr*(1+T/(2*Ti)+Td/T);
 
-z = tf('z');
-Rz = (r2*z^(-2)+r1*z^(-1)+r0)/(1-z^(-1));
-Gz = (W_kon(1)*z^(-6)+W_kon(2)*z^(-7))/(1-W_kon(3)*z^(-1)-W_kon(4)*z^(-2));
-G = feedback(Gz, Rz);
+E = zeros(T_sim, 1);
+Y = zeros(T_sim, 1);
+U = zeros(T_sim, 1);
+Y_zad = y_zad*ones(T_sim, 1);
+Y_zad(1:7) = zeros(7, 1);
 
-clf;
+for k = 8:T_sim
+    Y(k) = W_kon(1)*U(k-6)+W_kon(2)*U(k-7)+W_kon(3)*Y(k-1)+W_kon(4)*Y(k-2);
+    E(k) = Y_zad(k) - Y(k);
+    U(k) = r2*E(k-2)+r1*E(k-2)+r0*E(k)+U(k-1);
+end
+
+figure;
+subplot(211);
 hold on;
-plot(step(G, 200));
-xlabel('t');
-legend('y_{step}');
-print('-dpng', '../images/z3_p.png');
+plot(Y);
+plot(Y_zad, '--');
 hold off;
+legend('Y', 'Y_z_a_d', 'Location', 'southeast');
+xlabel('t');
+ylabel('Wyjscie modelu');
+
+subplot(212);
+hold on;
+plot(U);
+xlabel('t');
+ylabel('Sterowanie');
+legend('U', 'Location', 'northeast');
+hold off;
+
+print('-dpng', '../images/z3.png');
